00c7a4348d1459246b3be8a6fb8461ade76cfea8
Uwe Kamper <me@uwekamper.de>
2012-07-07 02:43:03 +0200

    startet building the PartyPlaylist model

--- backend/deejz/party/models.py
+++ backend/deejz/party/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 class PartyPlaylist(models.Model):
-	name = models.TextField()
+	name = models.TextField(blank=False, default="It's my party.")
+	address = models.TextField(blank=True)
+	slug = models.SlugField(blank=False, default='')
+	longitude = models.FloatField(blank=True, null=True)
+	latitude = models.FloatField(blank=True, null=True)
